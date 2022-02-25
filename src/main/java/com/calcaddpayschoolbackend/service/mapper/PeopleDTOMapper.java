08073935bd6235793532660c05a10925f1fa3612
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-02-25 10:15:38 +0300

    Add PeopleDTO

--- src/main/java/com/calcaddpayschoolbackend/service/mapper/PeopleDTOMapper.java
+++ src/main/java/com/calcaddpayschoolbackend/service/mapper/PeopleDTOMapper.java
@@ -1,4 +1,16 @@
 package com.calcaddpayschoolbackend.service.mapper;
 
-public class PeopleDTOMapper {
+import com.calcaddpayschoolbackend.dto.PeopleDTO;
+import com.calcaddpayschoolbackend.entity.People;
+
+public class PeopleDTOMapper implements EntityToDTOMapper<People, PeopleDTO> {
+    @Override
+    public PeopleDTO toDTO(People entity, Object... args) {
+        return null;
+    }
+
+    @Override
+    public People toEntity(PeopleDTO dto, Object... args) {
+        return null;
+    }
 }
